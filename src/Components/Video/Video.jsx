004e9e5627d774a016233c8c5fe3416cce99df30
Chidiebere95 <chidiezeokwelume@gmail.com>
2024-07-18 14:31:13 +0100

    update video slider on media page

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Video.css';
 import Title from '../../Views/Atoms/Title/Title';
 import ButtonPill from '../../Views/Molecules/ButtonPill/ButtonPill';
@@ -9,6 +9,43 @@ const Video = () => {
   const [direction, setDirection] = useState('left');
   const [active, setActive] = useState(1);
   const { play, expand } = useIcons();
+  const items = [
+    {
+      id: 1,
+      title: 'Short Inspirational Clips:',
+      info: 'Uplifting and thought-provoking short videos on various topics related to faith, social justice, and living a Christ-like life. Perfect for social media sharing and quick bursts of inspiration.',
+    },
+    {
+      id: 2,
+      title: 'Documentaries',
+      info: 'Compelling documentaries showcasing the impact of The Moving Bible movement around the world. Witness firsthand how faith translates into action and empowers positive change.',
+    },
+    {
+      id: 3,
+      title: 'How-To" Videos',
+      info: 'Practical tutorials on various topics like Bible study methods, prayer techniques, or incorporating faith into daily routines. These videos equip viewers with tools to deepen their spiritual growth',
+    },
+  ];
+  const [activeItem, setActiveItem] = useState({});
+  const handleActive = (direction) => {
+    setDirection(direction);
+    if (direction === 'left') {
+      if (active === 1) {
+        setActive(3);
+      } else {
+        setActive(active - 1);
+      }
+    } else {
+      if (active === 3) {
+        setActive(1);
+      } else {
+        setActive(active + 1);
+      }
+    }
+  };
+  useEffect(() => {
+    setActiveItem(items[active - 1]);
+  }, [active]);
   return (
     <div className='video-component'>
       <div className='container'>
@@ -18,33 +55,28 @@ const Video = () => {
           </div>
           <div className='details-wrapper'>
             <div className='details'>
-              <h2 className='title'>Short Inspirational Clips:</h2>
-              <p className='info'>
-                Uplifting and thought-provoking short videos on various topics
-                related to faith, social justice, and living a Christ-like life.
-                Perfect for social media sharing and quick bursts of
-                inspiration.
-              </p>
-                <div className='btns-wrapper'>
-            <ButtonPill
-              onClick={() => {
-                console.log('btn pill');
-              }}
-              text={'Watch now'}
-            />
-            <div className='btns'>
-              <ButtonRound
-                active={direction === 'left'}
-                onClick={() => setDirection('left')}
-                direction='left'
-              />
-              <ButtonRound
-                active={direction === 'right'}
-                onClick={() => setDirection('right')}
-                direction='right'
-              />
-            </div>
-          </div>
+              <h2 className='title'>{activeItem.title}</h2>
+              <p className='info'>{activeItem.info}</p>
+              <div className='btns-wrapper'>
+                <ButtonPill
+                  onClick={() => {
+                    console.log('btn pill');
+                  }}
+                  text={'Watch now'}
+                />
+                <div className='btns'>
+                  <ButtonRound
+                    active={direction === 'left'}
+                    onClick={() => handleActive('left')}
+                    direction='left'
+                  />
+                  <ButtonRound
+                    active={direction === 'right'}
+                    onClick={() => handleActive('right')}
+                    direction='right'
+                  />
+                </div>
+              </div>
             </div>
             <div className='video-wrapper'>
               <div className='video-con'>
@@ -60,7 +92,7 @@ const Video = () => {
               </div>
             </div>
           </div>
-        
+
           <div className='btns-2'>
             <div
               className={`btn ${active === 1 && 'active'}`}
