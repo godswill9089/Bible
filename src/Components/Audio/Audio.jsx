d4aeb7dbd2750768f7e9b068a306906bafd74d91
godswill9089 <ejimoforgodswill60@gmail.comm>
2024-07-18 12:51:19 +0100

    adjust img position on product section

--- src/Components/Audio/Audio.jsx
+++ src/Components/Audio/Audio.jsx
@@ -71,65 +71,67 @@ const Audio = () => {
   return (
     <div className='audio-component'>
       <div className='container'>
-        <div className='content'>
-          <div className='title-wrapper'>
-            <Title text={'Audio'} />
-          </div>
-          <div className='info'>
-            <p>
-              <span> Podcast: "Faith in Action": </span>Explore inspiring
-              stories of individuals who translate their faith into impactful
-              service. Hear interviews with pastors, missionaries, and everyday
-              heroes making a difference in their communities.
-            </p>
-            <p>
-              <span>Bible Studies:</span> Join in-depth discussions on scripture
-              with engaging hosts and guest speakers. Delve deeper into the
-              Bible's message and gain practical insights for daily living.
-            </p>
-          </div>
-          <div className='carousel'>
-            <div className='podcasts-wrapper'>
-              <div
-                className='podcasts'
-                style={{ transform: `translateX(-${translateSize}rem)` }}
-              >
-                {podcasts.map((podcast, index) => (
-                  <div key={index} className='podcast'>
-                    <div className='podcast-content'>
-                      <div className='img-wrapper'>
-                        <img className='img-1' src={podcast.img1} alt='' />
-                        <img className='img-2' src={podcast.img2} alt='' />
-                      </div>
-                      <div className='details'>
-                        <h3>{podcast.title}</h3>
-                        <p className='podcast-info'>{podcast.info}</p>
-                        <p className='podcast-text'>{podcast.text}</p>
-                      </div>
-                      <div className='btn-con'>
-                        <ButtonPill text={'Listen now'} podcast />
+        <div className='content-wrapper'>
+          <div className='content'>
+            <div className='title-wrapper'>
+              <Title text={'Audio'} />
+            </div>
+            <div className='info'>
+              <p>
+                <span> Podcast: "Faith in Action": </span>Explore inspiring
+                stories of individuals who translate their faith into impactful
+                service. Hear interviews with pastors, missionaries, and everyday
+                heroes making a difference in their communities.
+              </p>
+              <p>
+                <span>Bible Studies:</span> Join in-depth discussions on scripture
+                with engaging hosts and guest speakers. Delve deeper into the
+                Bible's message and gain practical insights for daily living.
+              </p>
+            </div>
+            <div className='carousel'>
+              <div className='podcasts-wrapper'>
+                <div
+                  className='podcasts'
+                  style={{ transform: `translateX(-${translateSize}rem)` }}
+                >
+                  {podcasts.map((podcast, index) => (
+                    <div key={index} className='podcast'>
+                      <div className='podcast-content'>
+                        <div className='img-wrapper'>
+                          <img className='img-1' src={podcast.img1} alt='' />
+                          <img className='img-2' src={podcast.img2} alt='' />
+                        </div>
+                        <div className='details'>
+                          <h3>{podcast.title}</h3>
+                          <p className='podcast-info'>{podcast.info}</p>
+                          <p className='podcast-text'>{podcast.text}</p>
+                        </div>
+                        <div className='btn-con'>
+                          <ButtonPill text={'Listen now'} podcast />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-              <div className='carousel-btn-left'>
-                <ButtonRound
-                  active={direction === 'left'}
-                  direction='left'
-                  onClick={() => handleTranslate('left')}
-                />
-              </div>
-              <div className='carousel-btn-right'>
-                <ButtonRound
-                  active={direction === 'right'}
-                  direction='right'
-                  onClick={() => handleTranslate('right')}
-                />
+                  ))}
+                </div>
+                <div className='carousel-btn-left'>
+                  <ButtonRound
+                    active={direction === 'left'}
+                    direction='left'
+                    onClick={() => handleTranslate('left')}
+                  />
+                </div>
+                <div className='carousel-btn-right'>
+                  <ButtonRound
+                    active={direction === 'right'}
+                    direction='right'
+                    onClick={() => handleTranslate('right')}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+      </div>
       </div>
     </div>
   );
--- src/Views/Molecules/CardMedia/CardMedia.jsx
+++ src/Views/Molecules/CardMedia/CardMedia.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './CardMedia.css';
-import { FaArrowRight } from 'react-icons/fa';
 import ButtonPill from '../ButtonPill/ButtonPill';
 
 const CardMedia = ({ title, info, img, onClick }) => {
@@ -11,12 +10,16 @@ const CardMedia = ({ title, info, img, onClick }) => {
         <div className='icon'>
           <img src={img} alt='e-book' className='' />
         </div>
-        <div className='details'>
-          <h1>{title}</h1>
-          <p>{info}</p>
-        </div>
-        <div className='btn-learn-more'>
-          <ButtonPill onClick={onClick} text={'Learn more'} />
+        <div className='details-wrapper'>
+          <div className='details'>
+            <h1>{title}</h1>
+            <p>{info}</p>
+          </div>
+          <div className='btn-learn-more-wrapper'> 
+            <div className='btn-learn-more'>
+              <ButtonPill onClick={onClick} text={'Learn more'} />
+            </div>
+         </div>
         </div>
       </div>
     </div>
